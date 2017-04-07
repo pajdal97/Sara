9315caf46f47794b98baef19ea14be97536e7fd8
Martin Nečas <martin.ocasek@email.cz>
2017-04-07 23:36:28 +0200

    list of lights

--- Application/app/src/main/java/com/example/marti/myapplication/LedListItemAdapter.java
+++ Application/app/src/main/java/com/example/marti/myapplication/LedListItemAdapter.java
@@ -1,50 +1,58 @@
 package com.example.marti.myapplication;
 
-import android.app.LauncherActivity;
 import android.content.Context;
-import android.graphics.Color;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.util.concurrent.LinkedTransferQueue;
-
-public class LedListItemAdapter extends ArrayAdapter<LedListItem>
-{
+public class LedListItemAdapter extends ArrayAdapter<LedListItem> {
     private int layoutResource;
 
-    public LedListItemAdapter(Context context, int layoutResource)
-    {
+    public LedListItemAdapter(Context context, int layoutResource) {
         super(context, layoutResource);
         this.layoutResource = layoutResource;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         View view = convertView;
 
-        if (view == null)
-        {
+        if (view == null) {
             LayoutInflater layoutInflater = LayoutInflater.from(getContext());
             view = layoutInflater.inflate(layoutResource, null);
         }
 
         LedListItem listItem = getItem(position);
 
-        if (listItem != null)
-        {
-            TextView ledTV = (TextView)view.findViewById(R.id.ledTV);
-            ledTV.setText("Light "+listItem.id);
+        if (listItem != null) {
+            TextView ledTV = (TextView) view.findViewById(R.id.ledTV);
+            ledTV.setText("Light " + listItem.id);
+            Switch sw = (Switch) view.findViewById(R.id.ledSwitch);
+            sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                    SharedPreferences pref = getContext().getSharedPreferences("User", 0);
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString("lightType", String.valueOf(position));
+                    if (isChecked) {
+                        editor.putString("switchType","ON");
+                    } else {
+                        editor.putString("switchType","OFF");
+                    }
+                    editor.apply();
+                    Intent postState = new Intent(getContext(), POSTtoMainServer.class);
+                    getContext().startService(postState);
+                    getContext().stopService(postState);
+                }
+            });
         }
 
         return view;
     }
-
 }
