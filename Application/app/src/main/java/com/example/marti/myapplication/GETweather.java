df441198affbb658a3fc9824ed26167aa95bf9e2
Martin Nečas <martin.ocasek@email.cz>
2017-04-07 14:21:44 +0200

    get weather

--- Application/app/src/main/java/com/example/marti/myapplication/GETweather.java
+++ Application/app/src/main/java/com/example/marti/myapplication/GETweather.java
@@ -3,12 +3,37 @@ package com.example.marti.myapplication;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class GETweather extends Service {
     public GETweather() {
     }
 
-    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=Brno&appid=507113590c3c266513b9a946bfd30156";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        queue.add(stringRequest);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+        @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
--- Application/app/src/main/java/com/example/marti/myapplication/MainActivity.java
+++ Application/app/src/main/java/com/example/marti/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.marti.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent getService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getService = new Intent(this, GETweather.class);
+        startService(getService);
+        stopService(getService);
     }
 }
