07de10eacc620b5ad618e5ffc4a5e1e4ea2a5b6b
ggjimmy <jirkazboril4@seznam.cz>
2017-04-08 03:16:11 +0200

    ALarm working within few features :))

--- Application/app/src/main/java/com/example/marti/myapplication/Texts.java
+++ Application/app/src/main/java/com/example/marti/myapplication/Texts.java
@@ -7,15 +7,23 @@ package com.example.marti.myapplication;
 public class Texts {
     private String ym;//years and month
     private String hm;//hours and minutes
+    private long secondsRemain;
 
-    public Texts(String ym, String hm){
+    public Texts(String ym, String hm,long secondsRemain){
         this.ym = ym;
         this.hm = hm;
+        this.secondsRemain = secondsRemain;
     }
     public String getYm(){
         return this.ym;
     }
     public String getHm(){
         return this.hm;
+    }
+    public long getSecondsRemain(){
+        return secondsRemain;
+    }
+    public void setSecondsRemain(long seconds){
+        this.secondsRemain = seconds;
     }
 }
