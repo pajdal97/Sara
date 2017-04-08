33b2cba788e6b857a0203b9e0035e80d77a65a0e
Unknown <antonin.mrkvica@outlook.cz>
2017-04-08 11:07:37 +0200

    khfg

---
+++ arduino/sketch_apr07a/sketch_apr07a.ino
@@ -0,0 +1,168 @@
+#include <SPI.h>
+#include <WiFi.h>
+
+#define LED RED_LED
+char ssid[] = "Sara Network";          //  your network SSID (name) 
+char pass[] = "saras123";   // your network password
+
+int status = WL_IDLE_STATUS;
+IPAddress server(192,168,1,238);
+
+// Initialize the client library
+WiFiClient client;
+
+void setup() { 
+  Serial.begin(9600);
+  Serial.println("Attempting to connect to WPA network...");
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+
+  while(true){
+  status = WiFi.begin(ssid, pass);
+  Serial.println(status);
+  if ( status != WL_CONNECTED) { 
+    Serial.println("Couldn't get a wifi connection");
+  } 
+  else {
+    Serial.println("Connected to wifi");
+    Serial.println("\nStarting connection...");
+    // if you get a connection, report back via serial:
+    while (true){
+    if (client.connect(server, 5000)) {
+        Serial.println("Connected");
+        // Make a HTTP request:
+        client.println("GET /");
+        client.println();
+        String c = client.readString();
+        Serial.println(c);
+        for (int i=0; i<c.length(); i++){
+          
+            if (i == 3){
+              String state =  c.substring(i-3,i);
+              
+              String on = "ON ";
+              String off = "OFF";
+              if  (state.equals(on)){
+                pinMode(4,OUTPUT);
+                digitalWrite(4, HIGH);
+              }
+
+              
+              else if (state.equals(off)) {
+                pinMode(4,OUTPUT);
+                digitalWrite(4, LOW);}
+              else{
+                Serial.println("Někde je asi chyba");
+              }
+            }
+           if (i == 7){
+              String state =  c.substring(i-3,i);
+          
+              String on = "ON ";
+              String off = "OFF";
+              if  (state.equals(on)){
+                pinMode(6,OUTPUT);
+                digitalWrite(6, HIGH);
+              }
+
+              
+              else if (state.equals(off)) {
+                pinMode(6,OUTPUT);
+                digitalWrite(6, LOW);}
+              else{
+                Serial.println("Někde je asi chyba");
+              }
+            }
+            
+            
+            if (i == 11){
+              String state =  c.substring(i-3,i);
+              
+              String on = "ON ";
+              String off = "OFF";
+              if  (state.equals(on)){
+                pinMode(8,OUTPUT);
+                digitalWrite(8, HIGH);
+              }
+
+              
+              else if (state.equals(off)) {
+                pinMode(8,OUTPUT);
+                digitalWrite(8, LOW);}
+              else{
+                Serial.println("Někde je asi chyba");
+              }
+            }
+        if (i == 15){
+         
+              String state =  c.substring(i-3,i);
+              String on = "ON ";
+              String off = "OFF";
+              if  (state.equals(on)){
+                pinMode(9,OUTPUT);
+                digitalWrite(9, HIGH);
+              
+              }
+              
+
+              
+              else if (state.equals(off)) {
+                pinMode(9,OUTPUT);
+                digitalWrite(9, LOW);}
+              else{
+                Serial.println("Někde je asi chyba");
+               
+               
+              }
+              
+            
+            
+          
+         
+          
+        }
+          
+         
+if (i == 19){
+         
+              String state =  c.substring(i-3,i);
+              Serial.println(state);
+              String on = "ON ";
+              String off = "OFF";
+              if  (state.equals(on)){
+                pinMode(10,OUTPUT);
+                digitalWrite(10, HIGH);
+              
+              }
+              
+
+              
+              else if (state.equals(off)) {
+                pinMode(10,OUTPUT);
+                digitalWrite(10, LOW);}
+              else{
+                Serial.println("Někde je asi chyba");
+               
+               
+              }
+              
+            
+            
+          
+         
+          
+        }
+        
+        
+        }
+                  }}
+      break;
+      
+    }
+  }
+}
+
+
+void loop() {
+
+}
--- python-server/server.py
+++ python-server/server.py
@@ -1,11 +1,13 @@
-from flask import *
+import flask
 import socket
+import urllib3
 
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
+
 
 class Light:
     def __init__(self, id=0):
@@ -25,34 +27,103 @@ class Light:
 svetlo1 = Light("0")
 svetlo2 = Light("1")
 svetlo3 = Light("2")
+svetlo4 = Light("3")
+svetlo5 = Light("4")
 
 lights = []
-lights.extend([svetlo1, svetlo2, svetlo3])
+lights.extend([svetlo1, svetlo2, svetlo3, svetlo4, svetlo5])
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route("/lights/API", methods=(["GET", "POST"]))
 def light():
-    if request.method == "GET":
+    if flask.request.method == "GET":
         hashmap = ({"id": svetlo1.get_id(), "state": svetlo1.get_state()},
                    {"id": svetlo2.get_id(), "state": svetlo2.get_state()},
-                   {"id": svetlo3.get_id(), "state": svetlo3.get_state()},)
-        jsonobj = json.dumps(hashmap)
+                   {"id": svetlo3.get_id(), "state": svetlo3.get_state()},
+                   {"id": svetlo4.get_id(), "state": svetlo4.get_state()},
+                   {"id": svetlo5.get_id(), "state": svetlo5.get_state()})
+        jsonobj = flask.json.dumps(hashmap)
         return jsonobj
 
-    elif request.method == "POST":
-        slovnik = json.dumps(request.form)
-        slovnik = json.loads(slovnik)
-        id_svetla = int(slovnik["id"])
-        lights[id_svetla].change_state(slovnik["state"])
+    elif flask.request.method == "POST":
+        slovnik = flask.json.dumps(flask.request.form)
+        slovnik = flask.json.loads(slovnik)
+        print(slovnik)
+        id_svetla = int(slovnik["id"][0])
+        zmena = slovnik["state"]
+        print(zmena)
+        if zmena[0] == "ON":
+            zmena[0] = "ON "
+        lights[id_svetla].change_state(zmena[0])
+        """
+        http = urllib3.PoolManager()
+        url = 'http://192.168.1.123:80'
+        fields = str(svetlo1.get_state()) + " " + str(svetlo2.get_state()) + " " + str(
+            svetlo3.get_state()) + " "
+        response = http.request('POST', url, headers={"Content-Type":"text/plain"}, fields="sdfjhd")
+        """
+        http = urllib3.PoolManager()
+        if zmena[0] == "ON ":
+            status = "on"
+        else:
+            status = "off"
+        url = 'http://10.10.4.127:8080/api/action?token=easy_token&type=light&status=' + status + '&find_by=id&find_v=' + str(
+            id_svetla + 1)
+        response = http.request('GET', url)
         return "status světla byl změněn"
 
 
-@app.route("/user/API")
+@app.route("/user/API", methods=(["GET", "POST"]))
 def user():
     pass
 
+
+@app.route("/door/api", methods=(["GET", "POST"]))
+def door():
+    if flask.request.method == "GET":
+        http = urllib3.PoolManager()
+        url = 'http://10.10.4.127:8080/api/get?token=easy_token&type=door&find_by=type'
+        hashmap = http.request('GET', url)
+        return hashmap
+
+    elif flask.request.method == "POST":
+        slovnik = flask.json.dumps(flask.request.form)
+        slovnik = flask.json.loads(slovnik)
+        print(slovnik)
+        if slovnik["state"][0] == "ON":
+            zmena = "on"
+        else:
+            zmena = "off"
+
+        print(zmena)
+        http = urllib3.PoolManager()
+        url = 'http://10.10.4.127:8080/api/action?token=easy_token&type=light&status=' + zmena + '&find_by=id&find_v=' + \
+              str(slovnik["id"][0])
+        response = http.request('GET', url)
+        return "změna dvěři"
+
+
+@app.route("/", methods=(["GET", "POST"]))
+def get():
+    return str(svetlo1.get_state()) + " " + str(svetlo2.get_state()) + " " + str(
+        svetlo3.get_state()) + " " + str(svetlo4.get_state()) + " " + str(svetlo5.get_state()) + " "
+
+
+@app.route("/api/action", methods=(["GET", "POST"]))
+def apiget():
+    id = flask.request.args.get("id")
+    state = flask.request.args.get("state")
+    if state == "on":
+        state = "ON "
+    else:
+        state = "OFF"
+    lights[int(id) - 1].change_state(state)
+    resp = flask.Response("OK")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+    return
 
 
 app.run(get_ip_address())
